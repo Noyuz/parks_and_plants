7c576e6aa3a22869c0e20d6c380b504cdf8d4786
Noyuz <k.abergel91@gmail.com>
2021-10-01 16:43:27 +0200

    pushing

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -10,6 +10,15 @@ class PlantsController < ApplicationController
     end
   end
 
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    @garden = @plant.garden
+
+    redirect_to garden_path(@garden)
+  end
+
+
   private
 
   def plant_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'plants/create'
   resources :gardens do
     resources :plants, only: :create
   end
+  resources :plants, only: :destroy
+  #delete '/plants/:id', to: 'plants#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
